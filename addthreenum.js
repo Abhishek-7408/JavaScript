226856d76dfbf247bd7a07481d1ac40ffdbd93af
Abhishek Kushwaha <105623789+Abhishek-7408@users.noreply.github.com>
2023-04-08 22:23:41 +0530

    Added

---
+++ addthreenum.js
@@ -0,0 +1,7 @@
+console.log('this program add three numbers');
+function addNumbers(a,b,c){
+    return a+b+c
+}
+
+let c = addNumbers(55,55,60);
+console.log(c);
---
+++ prototype.js
@@ -0,0 +1,5 @@
+console.log("this is a prototype file")
+let a = 67
+console.log(a)
+a = "Abhishek"
+console.log(a)
